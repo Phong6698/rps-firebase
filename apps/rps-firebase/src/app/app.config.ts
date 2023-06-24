f4288303e8341d8600657398c624df6a1a7cb023
Phong6698 <Phong@hotmail.ch>
2023-06-24 23:48:47 +0200

    add @angular/fire

--- apps/rps-firebase/src/app/app.config.ts
+++ apps/rps-firebase/src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
-import { appRoutes } from './app.routes';
+import {appRoutes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getFunctions, provideFunctions} from '@angular/fire/functions';
+import {getStorage, provideStorage} from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation(),),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(provideStorage(() => getStorage()))
+  ],
 };
---
+++ apps/rps-firebase/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'rps-firebase-724d6',
+    appId: '1:292270422048:web:026fcffd731d5793f7a445',
+    storageBucket: 'rps-firebase-724d6.appspot.com',
+    locationId: 'europe-west6',
+    apiKey: 'AIzaSyBZ51wUZIY6cIH0o14JC2c_9-t5G6EYxeg',
+    authDomain: 'rps-firebase-724d6.firebaseapp.com',
+    messagingSenderId: '292270422048',
+  },
+};
